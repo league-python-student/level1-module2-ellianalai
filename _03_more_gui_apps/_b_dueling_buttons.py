e8349eb4d777383e439539aafee8bd6e2028382d
root <root@gmail.com>
2022-01-29 19:29:51 +0000

    Working on Calculator

--- _03_more_gui_apps/_b_dueling_buttons.py
+++ _03_more_gui_apps/_b_dueling_buttons.py
@@ -11,14 +11,34 @@ RIGHT_SMALL = {'relx' : 0.8, 'relwidth' : 0.2}
 # ======================= DO NOT EDIT THE CODE ABOVE =========================
 
 # TODO 1) Create a new tkinter class
+class button(tk.Tk):
+
+
 
     # TODO 2) Create a constructor
-
+    def __init__(self):
     # TODO 3) call Tk's constructor,
+        super().__init__()
+        self.button1 = tk.Button(self, text='Click me!', fg='red',
+                             font=('Courier new', 200, 'bold'), command=self.left_button_pressed)
 
+        self.button1.place(relx=0.1, rely=0.2   , relwidth=0.8, relheight=0.2)
+
+        self.button2 = tk.Button(self, text='Click me!!', fg='blue',
+                             font=('Courier new', 20, 'bold'), command=self.right_button_pressed)
+        self.button2.place(relx=0.1, rely=0.5, relwidth=0.8, relheight=0.2)
     # TODO 4) Create 2 buttons next to each other, side by side, with the
     #  text "Click me!". Make sure the buttons are member variables and
     #  look at the example photo for reference.
+    def left_button_pressed(self):
+        self.button1.place(LEFT_SMALL)
+        self.button2.place(RIGHT_BIG)
+
+
+    def right_button_pressed(self):
+        self.button1.place(LEFT_BIG)
+        self.button2.place(RIGHT_SMALL)
+
 
     # TODO 5) For the left button, add: command=self.left_button_pressed
     #  For the right button, add: command=self.right_button_pressed
@@ -33,9 +53,11 @@ RIGHT_SMALL = {'relx' : 0.8, 'relwidth' : 0.2}
 
 
 # TODO 8) Create an if __name__ == '__main__': block
+if __name__ == '__main__':
 
     # TODO 9) Create an object of the dueling buttons class
-
+    press = button()
     # TODO 10) Set the object's width and size using the geometry method
-
+    press.geometry('500x500')
     # TODO 11) Call the object's mainloop method
+    press.mainloop()
--- _03_more_gui_apps/_c_calculator.py
+++ _03_more_gui_apps/_c_calculator.py
@@ -2,7 +2,36 @@
 Create a simple calculator app
 """
 import tkinter as tk
+class calculator(tk.Tk):
 
 # TODO Make a calculator app like the one shown in the calculator.png image
 #  located in this folder.
 #  HINT: you'll need: 2 text fields, 4 buttons, and 1 label
+    def __init__(self):
+        super().__init__()
+#What to put in text??? Create a function?
+        self.label = tk.Label(self, text="???",
+                    fg='blue', font=('Times New Roman', 40, 'bold'))
+        self.label.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.2)
+#Have to create a function for command = self.______& change x,y,width,height
+        self.button = tk.Button(self, text='Add', fg='black',
+                                font=('courier new', 16, 'bold'), command=self.on_button_press)
+        self.button.place(relx=0.1, rely=0.5, relwidth=0.8, relheight=0.2)
+        self.button = tk.Button(self, text='Subtract', fg='black',
+                                font=('courier new', 16, 'bold'), command=self.on_button_press)
+        self.button.place(relx=0.1, rely=0.5, relwidth=0.8, relheight=0.2)
+        self.button = tk.Button(self, text='Multiply', fg='black',
+                                font=('courier new', 16, 'bold'), command=self.on_button_press)
+        self.button.place(relx=0.1, rely=0.5, relwidth=0.8, relheight=0.2)
+        self.button = tk.Button(self, text='Divide', fg='black',
+                                font=('courier new', 16, 'bold'), command=self.on_button_press)
+        self.button.place(relx=0.1, rely=0.5, relwidth=0.8, relheight=0.2)
+#Text Fields, how do I connect this with the button press and label?
+        self.text_field = tk.Entry(self)
+        self.text_field.place(relx=0.1, rely=0.8, relwidth=0.8, height=18)
+        self.text_field = tk.Entry(self)
+        self.text_field.place(relx=0.1, rely=0.8, relwidth=0.8, height=18)
+if __name__ == '__main__':
+    calc = calculator()
+    calc.geometry('500x500')
+    calc.mainloop()
